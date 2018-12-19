f134c596fadc4ee645eded9f933bd0111aeb66dc
kouzt123 <kouzt123@gmail.com>
2018-12-18 23:44:58 -0800

    update

---
+++ EconLawPresentation/config.js
@@ -0,0 +1,1 @@
+IDRViewer.config = {"pagecount":1,"title":"","author":"","fileName":"Econ_Law_Presentation.pdf","bounds":[[935,1210]],"bookmarks":[],"thumbnailType":"jpg","pageType":"html","pageLabels":[]};
